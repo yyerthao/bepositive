95a4633dce6014e297e0d95fd1bd16d36f74bef5
Yer Thao <maivyerthao@gmail.com>
2021-02-22 15:34:43 -0600

    Converted InfoPage to Messages

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -31,8 +31,8 @@ const Nav = (props) => {
         {/* Show the link to the info page and the logout button if the user is logged in */}
         {props.store.user.id && (
           <>
-            <Link className="nav-link" to="/info">
-              Info Page
+            <Link className="nav-link" to="/messages">
+              Messages
             </Link>
             <LogOutButton className="nav-link" />
           </>
--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -4,17 +4,27 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
 class UserPage extends Component {
-  // this component doesn't do much to start, just renders some user info to the DOM
   render() {
     return (
       <div>
-        <h1 id="welcome">Welcome, {this.props.store.user.username}!</h1>
-        <p>Your ID is: {this.props.store.user.id}</p>
-        <LogOutButton className="log-in" />
+        <center>
+          <h1 id="welcome">Welcome, {this.props.store.user.username}!</h1>
+          <p>Your ID is: {this.props.store.user.id}</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.Quod, quae voluptatibus quisquam consectetur nemo 
+            laudantium ratione inventore nostrum magni doloribus similique maiores obcaecati officiis delectus, soluta 
+            officia explicabo dolorum repudiandae.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.Quod, quae voluptatibus quisquam consectetur nemo 
+            laudantium ratione inventore nostrum magni doloribus similique maiores obcaecati officiis delectus, soluta 
+            officia explicabo dolorum repudiandae.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.Quod, quae voluptatibus quisquam consectetur nemo 
+            laudantium ratione inventore nostrum magni doloribus similique maiores obcaecati officiis delectus, soluta 
+            officia explicabo dolorum repudiandae.
+          </p>
+        </center>
       </div>
     );
   }
 }
 
-// this allows us to use <App /> in index.js
 export default connect(mapStoreToProps)(UserPage);
