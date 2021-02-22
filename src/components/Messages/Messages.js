a2bc4a397da6e1daa0e60d3453cc4342a975ffde
Yer Thao <maivyerthao@gmail.com>
2021-02-22 16:48:44 -0600

    Get route working

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -5,9 +5,16 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 
 class Messages extends Component {
 
+componentDidMount(){
+  this.props.dispatch({type: 'FETCH_MESSAGE'});
+}
+
+
   render() {
+    const {messageReducer} = this.props.store;
     return (
       <div className="container">
+        {JSON.stringify(messageReducer)}
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit.Quod, quae voluptatibus quisquam consectetur nemo 
           laudantium ratione inventore nostrum magni doloribus similique maiores obcaecati officiis delectus, soluta 
