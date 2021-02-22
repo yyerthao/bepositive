1e8a7dbdcc5b3c39275e68d47f1eee19badc6e4e
Yer Thao <maivyerthao@gmail.com>
2021-02-22 17:25:19 -0600

    Converted object in messageReducer to array in order to map

--- server/routes/message.router.js
+++ server/routes/message.router.js
@@ -13,7 +13,7 @@ let sqlText =
     WHERE "user".id = $1;`
   pool.query(sqlText, [req.user.id])
     .then((result) => {
-      res.send(result.rows[0]);
+      res.send(result.rows);
     }).catch((error) => {
       console.log('Error fetching message', error);
       res.sendStatus(500);
