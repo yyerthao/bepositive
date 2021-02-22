e152779cffbd811cfbc754d6bbead66745e8bfb9
Yer Thao <maivyerthao@gmail.com>
2021-02-22 15:36:46 -0600

    Skeleton of components laid out

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,29 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
 
-const InfoPage = () => (
-  <div>
-    <p>Info Page</p>
-  </div>
-);
-
-// If you needed to add local state or other things,
-// you can make it a class component like:
-
-/*
-class InfoPage extends React.Component {
+class Messages extends Component {
 
   render() {
     return (
       <div>
-        <p>Info Page</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.Quod, quae voluptatibus quisquam consectetur nemo 
+          laudantium ratione inventore nostrum magni doloribus similique maiores obcaecati officiis delectus, soluta 
+          officia explicabo dolorum repudiandae.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.Quod, quae voluptatibus quisquam consectetur nemo 
+          laudantium ratione inventore nostrum magni doloribus similique maiores obcaecati officiis delectus, soluta 
+          officia explicabo dolorum repudiandae.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.Quod, quae voluptatibus quisquam consectetur nemo 
+          laudantium ratione inventore nostrum magni doloribus similique maiores obcaecati officiis delectus, soluta 
+          officia explicabo dolorum repudiandae.
+        </p>
       </div>
     )
   }
 }
-*/
-export default InfoPage;
+
+export default connect(mapStoreToProps)(Messages);
--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
 class UserPage extends Component {
