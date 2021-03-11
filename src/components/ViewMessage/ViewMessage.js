027854c91277c40c0d4e709ccaea5f5ed95b9cc8
Yer Thao <maivyerthao@gmail.com>
2021-03-10 19:55:13 -0600

    Added material ui paper to ViewMessage.js

--- src/components/ViewMessage/ViewMessage.js
+++ src/components/ViewMessage/ViewMessage.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import Swal from 'sweetalert2';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import PropTypes from 'prop-types';
+
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+});
 
 
 class ViewMessage extends Component {
@@ -43,28 +55,31 @@ class ViewMessage extends Component {
   //   this.props.history.push('/updateDream')
 
 
-  render(){
-    const {detailsReducer} = this.props.store;
-    return(
-      <div className="container">
-        {detailsReducer.map((details, i) => {
-          return(
-            <div key={i}>
-              <p>Name: {details.name}</p>
-              <img src={details.image} alt="happy things"></img>
-              <p>Details: {details.details}</p>
-              {/* <button onClick={this.deleteMessage(detailsReducer.id)}>Delete Message</button> */}
-              <button>Delete</button>
-            </div>
-          )
-        })}
-
-
-
-
+  render(props){
+    const { classes } = this.props;
+      const {detailsReducer} = this.props.store;
+      return(
+        <div className="container">
+          <Paper className={classes.root} elevation={1}>
+          {detailsReducer.map((details, i) => {
+            return(
+              <div key={i}>
+                <p>Name: {details.name}</p>
+                <img src={details.image} alt="happy things"></img>
+                <p>Details: {details.details}</p>
+                {/* <button onClick={this.deleteMessage(detailsReducer.id)}>Delete Message</button> */}
+                <button>Delete</button>
+              </div>
+            )
+          })}
+          </Paper>
       </div>
     )
   }
 }
 
-export default connect(mapStoreToProps)(ViewMessage);
+ViewMessage.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default connect(mapStoreToProps)(withStyles(styles)(ViewMessage));
