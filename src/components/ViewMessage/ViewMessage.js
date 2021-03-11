b42cd252ca33b9529ee8d588c60bdfe82f3160bc
Yer Thao <maivyerthao@gmail.com>
2021-03-10 20:40:06 -0600

    Updated material ui depracated warning

--- src/components/ViewMessage/ViewMessage.js
+++ src/components/ViewMessage/ViewMessage.js
@@ -10,8 +10,8 @@ import PropTypes from 'prop-types';
 const styles = theme => ({
   root: {
     ...theme.mixins.gutters(),
-    paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2,
+    paddingTop: theme.spacing() * 2,
+    paddingBottom: theme.spacing() * 2,
   },
 });
 
